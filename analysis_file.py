f9b0a1509b88c7b2711bcc74fc912e31de2bab9f
kyu90 <38861195+kyu90@users.noreply.github.com>
2018-11-24 10:18:24 +0900

    Create analysis_file.py

---
+++ analysis_file.py
@@ -0,0 +1,21 @@
+ksl = KnuSL
+
+print("\nKNU 한국어 감성사전입니다~ :)")
+print("사전에 단어가 없는 경우 결과가 None으로 나타납니다!!!")
+print("종료하시려면 #을 입력해주세요!!!")
+print("-2:매우 부정, -1:부정, 0:중립 or Unkwon, 1:긍정, 2:매우 긍정")
+print("\n")	
+
+wordname = df['description']
+result = []
+wordname = wordname.apply(lambda x: x.split(' '))
+for i in range(0, len(wordname)):
+    temp = wordname.iloc[i]
+    temp2 = 0
+    for j in range(0, len(temp)):
+        r_word, s_word = ksl.data_list(temp[j])
+        try:
+            temp2 = temp2+int(s_word)
+        except:
+            pass
+    result.append(temp2)
