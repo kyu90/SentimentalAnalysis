7b8fb3afa4522ec6ca4dc7f1013bfedde84f0cf0
kyu90 <38861195+kyu90@users.noreply.github.com>
2018-11-24 09:41:12 +0900

    Create KnuSL.py

---
+++ KnuSL.py
@@ -0,0 +1,22 @@
+import json
+
+class KnuSL():
+    def data_list(wordname):
+        with open('C:/Users/user/Desktop/Dev/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
+                data = json.load(f)
+        result = ['None','None']
+        for i in range(0, len(data)):
+            if data[i]['word'] == wordname:
+                result.pop()
+                result.pop()
+                result.append(data[i]['word_root'])
+                result.append(data[i]['polarity'])	
+
+        r_word = result[0]
+        s_word = result[1]
+
+        print('어근 : ' + r_word)
+        print('극성 : ' + s_word)
+
+
+        return r_word, s_word
